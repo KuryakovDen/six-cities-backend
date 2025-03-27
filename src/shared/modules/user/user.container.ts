c1c21b9c63165b79bc76102045853abdef22eb0e
KuryakovDen <fifer2@yandex.ru>
2025-03-27 22:51:16 +0700

    Обновление контейнеров

--- src/shared/modules/user/user.container.ts
+++ src/shared/modules/user/user.container.ts
@@ -4,6 +4,8 @@ import {UserService} from './user-service.interface.js';
 import {Component} from '../../types/component.enum.js';
 import {DefaultUserService} from './default-user.service.js';
 import {UserEntity, UserModel} from './user.entity.js';
+import {Controller} from '../../libs/rest/controller/controller.interface.js';
+import {UserController} from './user.controller.js';
 
 export function createUserContainer() {
   const userContainer = new Container();
@@ -16,6 +18,11 @@ export function createUserContainer() {
   userContainer
     .bind<types.ModelType<UserEntity>>(Component.UserModel)
     .toConstantValue(UserModel);
+
+  userContainer
+    .bind<Controller>(Component.UserController)
+    .to(UserController)
+    .inSingletonScope();
 
   return userContainer;
 }
